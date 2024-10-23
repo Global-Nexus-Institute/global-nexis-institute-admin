b98364634e9edd406b585e094dbbf06d356832d4
Elcid-Chawa <elcidchawa@gmail.com>
2024-10-23 13:55:02 +0200

    upadted users table

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,37 +1,33 @@
 "use client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
-import { Table, Button } from "antd";
-
-interface User {
-  _id: string;
-  name: string;
-  email: string;
-}
+import { Table, Button, TableColumnProps } from "antd";
+import { useAppSelector } from "@/lib/store/store.hooks";
+import { RootState } from "@/lib/store/store";
+import { useUsers } from "@/shared/hooks/users/users.hooks";
+import { UsersDataType } from "@/shared/types";
 
 const UsersPage: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<UsersDataType[]>([]);
+  const [refresh, setReferesh] = useState(false);
+
+  const { data: students, loading } = useAppSelector(
+    (state: RootState) => state.users,
+  );
+
+  const { getUsers } = useUsers();
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get("/api/users");
-        setUsers(response.data);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    };
+    getUsers();
+  }, [refresh]);
 
-    fetchUsers();
-  }, []);
-
-  const columns = [
-    { title: "Name", dataIndex: "name", key: "name" },
+  const columns: TableColumnProps<UsersDataType>[] = [
+    { title: "Name", dataIndex: "names", key: "name" },
     { title: "Email", dataIndex: "email", key: "email" },
     {
       title: "Action",
       key: "action",
-      render: (_: any, record: User) => (
+      render: (_: any, record: UsersDataType) => (
         <Button
           danger
           className="bg-red-500 text-white"
@@ -46,7 +42,7 @@ const UsersPage: React.FC = () => {
   const deleteUser = async (userId: string) => {
     try {
       await axios.delete(`/api/users/${userId}`);
-      setUsers(users.filter((user) => user._id !== userId));
+      setUsers(students.filter((user) => user._id !== userId));
     } catch (error) {
       console.error("Error deleting user:", error);
     }
@@ -55,7 +51,7 @@ const UsersPage: React.FC = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Manage Users</h1>
-      <Table columns={columns} dataSource={users} rowKey="_id" />
+      <Table columns={columns} dataSource={students} rowKey="_id" />
     </div>
   );
 };

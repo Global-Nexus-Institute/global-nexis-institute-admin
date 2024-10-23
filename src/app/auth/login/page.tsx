ea234b2ca30eba82debd0c1c82707bbd2de66930
Elcid-Chawa <elcidchawa@gmail.com>
2024-10-23 11:08:22 +0200

    added authentication

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,24 +1,42 @@
 "use client";
 import React, { useEffect } from "react";
 import LoginFrom from "./_components/login-form/LoginFrom";
-import { useAppSelector } from "@/lib/store/store.hooks";
+import { useAppDispatch, useAppSelector } from "@/lib/store/store.hooks";
 import { RootState } from "@/lib/store/store";
+import { Alert } from "antd";
+import { resetAuthState } from "@/lib/features/slices/auth.slice";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const { user, loading, error, successMesage } = useAppSelector(
     (store: RootState) => store.auth,
   );
 
+  const dispatch = useAppDispatch();
+
+  const closeAlert = () => {
+    dispatch(resetAuthState());
+  };
+
+  const router = useRouter();
+
   useEffect(() => {
     if (!loading && successMesage) {
-      alert(successMesage);
+      router.replace(`/?user=${user?._id}`);
     }
     if (!loading && error) {
       alert(error);
     }
-  }, [user, loading, error, successMesage]);
+  }, [loading, error, successMesage]);
   return (
-    <div className="flex flex-col w-full justify-center items-center bg-blue-900 h-[100%]">
+    <div className="flex flex-col w-full justify-center items-center bg-blue-900 h-full py-10">
+      {error && (
+        <Alert
+          message={error}
+          className="border border-2 border-red-500"
+          onClick={() => closeAlert()}
+        />
+      )}
       <LoginFrom />
     </div>
   );
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,24 +1,32 @@
 "use client";
-import React, { useEffect } from "react";
-import { useAppSelector } from "@/lib/store/store.hooks";
+import React, { use, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/store/store.hooks";
 import { RootState } from "@/lib/store/store";
 import SignUp from "./_components/SignUpForm";
+import { Alert } from "antd";
+import { resetAuthState } from "@/lib/features/slices/auth.slice";
 
 export default function SingUpPage() {
   const { user, loading, error, successMesage } = useAppSelector(
     (store: RootState) => store.auth,
   );
+  const dispatch = useAppDispatch();
+  const closeAlert = () => {
+    dispatch(resetAuthState());
+  };
 
   useEffect(() => {
     if (!loading && successMesage) {
       alert(successMesage);
     }
     if (!loading && error) {
-      alert(error);
+      // alert(error);
+      console.log("Captured error: ", error);
     }
-  }, [user, loading, error, successMesage]);
+  }, [loading, error, successMesage]);
   return (
-    <div className="flex flex-col w-full justify-center items-center bg-blue-900 h-screen">
+    <div className="flex flex-col w-full justify-center items-center bg-blue-900 h-screen py-10">
+      {error && <Alert message={error} onClick={() => closeAlert()} />}
       <SignUp />
     </div>
   );

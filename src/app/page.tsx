9988e4af47246ebf712aedd79cc3769d86e6a916
Elcid-Chawa <elcidchawa@gmail.com>
2024-10-23 17:38:12 +0200

    manage loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import { Avatar, Card, Col, Menu, Row, Statistic } from "antd";
+import { Avatar, Card, Col, Menu, Row, Spin, Statistic } from "antd";
 import {
   AppstoreOutlined,
   HomeFilled,
@@ -21,6 +21,7 @@ import { useAppSelector } from "@/lib/store/store.hooks";
 import { useAuth } from "@/shared/hooks/auth/auth.hooks";
 import { useDispatch } from "react-redux";
 import { resetAuthState } from "@/lib/features/slices/auth.slice";
+import { RouteLoader } from "@/components/route-loader/RouteLoader";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
@@ -60,6 +61,12 @@ export default function Home() {
 
   const router = useRouter();
 
+  const token = localStorage.getItem("LOCAL_USER_TOKEN");
+
+  useEffect(() => {
+    console.log("🚀 ~ file: page.tsx:useEffect ~ token:", token);
+  }, []);
+
   useEffect(() => {
     if (user === null) {
       const timer = setTimeout(() => {
@@ -86,105 +93,117 @@ export default function Home() {
 
   return (
     <div className="h-full">
-      <div className="flex h-20 bg-white">
-        <div className="flex rounded-full basis-1/5 border border-gray-300 m-3">
-          Logo
-        </div>
-        <div className="flex basis-3/5 h-full">
-          <Menu
-            onClick={onClick}
-            selectedKeys={[current]}
-            mode="horizontal"
-            items={items}
-            className="flex w-full items-center justify-center h-full"
-          />
+      <RouteLoader />
+      <Spin spinning={loading} size={"large"} tip={"..Looading"}>
+        <div className="flex h-20 bg-white">
+          <div className="flex rounded-full basis-1/5 border border-gray-300 m-3">
+            Logo
+          </div>
+          <div className="flex basis-3/5 h-full">
+            <Menu
+              onClick={onClick}
+              selectedKeys={[current]}
+              mode="horizontal"
+              items={items}
+              className="flex w-full items-center justify-center h-full"
+            />
+          </div>
+
+          <div className="flex justify-center items-center basis-1/5 border">
+            {user && (
+              <div className="flex gap-4">
+                <Avatar size={40}>{user.names[0]}</Avatar>{" "}
+                <div
+                  onClick={handLogOut}
+                  className="text-black text-xl cursor-pointer"
+                >
+                  Logout
+                </div>
+              </div>
+            )}
+          </div>
         </div>
 
-        <div className="flex justify-center items-center basis-1/5 border">
-          {user && (
-            <div className="flex gap-4">
-              <Avatar size={40}>{user.names[0]}</Avatar>{" "}
-              <div
-                onClick={handLogOut}
-                className="text-black text-xl cursor-pointer"
-              >
-                Logout
+        <div className="flex flex-1 flex-col justify-center ">
+          <div className="h-screen md:flex ">
+            <div className="flex flex-col pl-10 basis-1/2 bg-gray-600 h-full justify-center">
+              <div>
+                <Image
+                  src="/images/logo.png"
+                  width={200}
+                  height={200}
+                  alt="logo"
+                />
+              </div>
+              <div className="flex gap-4 ">
+                {user ? (
+                  <div>
+                    <h1 className="text-2xl font-bold mt-3 ">
+                      Welcome {user.names}
+                    </h1>
+                  </div>
+                ) : (
+                  <div className="mt-3 border p-3 bg-blue-900 rounded-lg ">
+                    <Link href="/auth/login">Login</Link>
+                  </div>
+                )}
               </div>
             </div>
-          )}
-        </div>
-      </div>
-
-      <div className="flex pl-10 flex-1 flex-col justify-center ">
-        <div className="h-screen md:flex">
-          <div className="flex flex-col basis-1/2 bg-gray-600 h-full justify-center">
-            <div>Logo</div>
-            <div className="flex gap-4 ">
-              {user ? (
-                <div>
-                  <h1 className="text-2xl font-bold mt-3 ">
-                    Welcome {user.names}
-                  </h1>
-                </div>
-              ) : (
-                <div className="mt-3 border p-3 bg-blue-900 rounded-lg ">
-                  <Link href="/auth/login">Login</Link>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="p-3 basis-1/2 h-full space-y-4">
-            <Row gutter={6}>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title="Active Students"
-                    value={1123}
-                    valueRender={(value) => <Link href="/users">{value}</Link>}
-                    prefix={<FontAwesomeIcon icon={faUsers} />}
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title="Available Courses"
-                    value={1123}
-                    valueRender={(value) => (
-                      <Link href="/courses">{value}</Link>
-                    )}
-                    prefix={<FontAwesomeIcon icon={faBook} />}
-                  />
-                </Card>
-              </Col>
-            </Row>
-            <Row gutter={6}>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title="Active Instructors"
-                    value={1123}
-                    prefix={<FontAwesomeIcon icon={faUsers} />}
-                  />
-                </Card>
-              </Col>
-              <Col span={12}>
-                <Card>
-                  <Statistic
-                    title="Available Lessons"
-                    value={1123}
-                    prefix={<FontAwesomeIcon icon={faBook} />}
-                  />
-                </Card>
-              </Col>
-            </Row>
-            <br />
-            <div className="h-auto bg-white">
-              <ColumnChart />
+            <div className="p-3 basis-1/2 h-full space-y-4">
+              <Row gutter={6}>
+                <Col span={12}>
+                  <Card>
+                    <Statistic
+                      title="Active Students"
+                      value={1123}
+                      valueRender={(value) => (
+                        <Link href="/users">{value}</Link>
+                      )}
+                      prefix={<FontAwesomeIcon icon={faUsers} />}
+                    />
+                  </Card>
+                </Col>
+                <Col span={12}>
+                  <Card>
+                    <Statistic
+                      title="Available Courses"
+                      value={1123}
+                      valueRender={(value) => (
+                        <Link href="/courses">{value}</Link>
+                      )}
+                      prefix={<FontAwesomeIcon icon={faBook} />}
+                    />
+                  </Card>
+                </Col>
+              </Row>
+              <Row gutter={6}>
+                <Col span={12}>
+                  <Card>
+                    <Statistic
+                      title="Active Instructors"
+                      value={1123}
+                      prefix={<FontAwesomeIcon icon={faUsers} />}
+                    />
+                  </Card>
+                </Col>
+                <Col span={12}>
+                  <Card>
+                    <Statistic
+                      title="Available Lessons"
+                      value={1123}
+                      prefix={<FontAwesomeIcon icon={faBook} />}
+                    />
+                  </Card>
+                </Col>
+              </Row>
+              <br />
+              <div className="h-auto bg-white">
+                <ColumnChart />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Spin>
     </div>
   );
 }

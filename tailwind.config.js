06ca80652633e28e4aab4e63c9d08c0b78209034
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-05 18:35:07 +0200

    added theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        gnpurple: "#A050C9",
+        gnblueLight: "#2EB9C2",
+        gntextblue: "#04F3D8",
+        gnpink: "#D0376E",
+        gnpinkdark: "#6A1C38",
+        gndarkblue: "#19173C",
+      },
+    },
   },
   plugins: [],
 };

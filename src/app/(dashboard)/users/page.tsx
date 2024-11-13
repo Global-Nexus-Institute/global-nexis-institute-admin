05ceffbe8b80f35877bb341c6401ab06d09ef30b
Elcid-Chawa <elcidchawa@gmail.com>
2024-11-13 23:38:00 +0200

    add skeleton loader to students

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Table, Button, TableColumnProps, Card, Col, Row, Statistic } from "antd";
+import {
+  Table,
+  Button,
+  TableColumnProps,
+  Card,
+  Col,
+  Row,
+  Statistic,
+  Skeleton,
+} from "antd";
 import { useAppSelector } from "@/lib/store/store.hooks";
 import { RootState } from "@/lib/store/store";
 import { useUsers } from "@/shared/hooks/users/users.hooks";
@@ -16,36 +25,37 @@ const UsersPage: React.FC = () => {
     (state: RootState) => state.users,
   );
 
-  const { getUsers } = useUsers();
+  const { getStudents } = useUsers();
 
   useEffect(() => {
-    getUsers();
+    getStudents();
   }, [refresh]);
-
 
   return (
     <div>
       <h1 className="text-2xl text-white font-bold mb-4">Manage Users</h1>
-      <Row className=" h-50 my-2 bodrder p-5 w-full gap-4" gutter={6}>
-        <Col span={6}>
-          <Card>
-            <Statistic title="Total Students" value={students.length} />
-          </Card>
-        </Col>
-        <Col span={6}>
-          {" "}
-          <Card>
-            <Statistic title="Total Enrolled" value={0} />
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card>
-            <Statistic title="Total Pending" value={0} />
-          </Card>
-        </Col>
-        <Col span={6}></Col>
-      </Row>
-      <UserTable students={students} />
+      <Skeleton loading={loading} active={loading}>
+        <Row className=" h-50 my-2 bodrder p-5 w-full gap-4" gutter={6}>
+          <Col span={6}>
+            <Card>
+              <Statistic title="Total Students" value={students.length} />
+            </Card>
+          </Col>
+          <Col span={6}>
+            {" "}
+            <Card>
+              <Statistic title="Total Enrolled" value={0} />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card>
+              <Statistic title="Total Pending" value={0} />
+            </Card>
+          </Col>
+          <Col span={6}></Col>
+        </Row>
+        <UserTable students={students} />
+      </Skeleton>
     </div>
   );
 };
